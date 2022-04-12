a686d0998d015c5c74151eeb04096b7ff7bfb7d1
Davin Glynn <glynnd@moravian.edu>
2022-04-11 20:03:05 -0400

    DistanceRoute

--- src/DistanceRoute.java
+++ src/DistanceRoute.java
@@ -10,13 +10,17 @@ public class DistanceRoute
     private ArrayList<Address> houses;  //List of houses
     private Address loc;  //current location
     public DistanceFinder dist;  //distancefinder object
+    //private MoveTruck truck;
+    private Truck truck;
 
     /**
      * Constructor
      * @param local     The current location of the truck
+     * @param t         The truck to move
      */
-    public DistanceRoute(Address local)
+    public DistanceRoute(Address local, Truck t)
     {
+        truck = t;
         loc = local;
         dist = new DistanceFinder();
         houses = new ArrayList<Address>();
@@ -55,6 +59,38 @@ public class DistanceRoute
         Collections.sort(houses, new DistanceCompare(loc));
     }
 
+    /**
+    public void moveNext()
+    {
+        Address next = houses.get(0);
+        System.out.println(next.getStreet() + "  " + loc.getStreet());
+        while(Character.valueoOf(next.getStreet()).compareTo(Character.valueOf(loc.getStreet())) != 0);
+        {
+            System.out.println("one");
+            if(next.getStreet() - loc.getStreet() > 0)
+            {
+                truck.moveTruck.moveTruckRight();
+            }
+            if(next.getStreet() - loc.getStreet() < 0)
+            {
+                truck.moveTruck.moveTruckLeft();
+            }
+        }
+        while(next.getNumber()/100 != loc.getNumber()/100)
+        {
+            System.out.println("two");
+            if(next.getNumber()/100 - loc.getNumber()/100 > 0)
+            {
+                truck.moveTruck.moveTruckDown();
+            }
+            if(next.getNumber()/100 - loc.getNumber()/100 < 0)
+            {
+                truck.moveTruck.moveTruckUp();
+            }
+        }
+
+    }
+**/
 
 
 }
